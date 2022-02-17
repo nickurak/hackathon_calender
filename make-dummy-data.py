b8b325336773e875251bab0fde3ea2a5e2d3fb01
Jeremy Nickurak <jnickurak@dotdash.com>
2022-02-17 11:33:05 -0700

    make-dummy-data.py: script to build dummy calendars.json and events.json files

---
+++ make-dummy-data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import json
+
+sample_registered_calendars = {
+    'name': 'hackathon test calendar',
+    'url': 'https://calendar.google.com/calendar/ical/c_adghr2e0nnjq9ve4a408lh8dc4%40group.calendar.google.com/public/basic.ics',
+}
+
+
+sample_events = {
+    'uid': 'some fake uid',
+    'event_name': 'test event',
+    'start_time': 1645124381,
+    'end_time': 1645124989
+}
+
+
+def main():
+    with open("calendars.json", "w") as outfile:
+        json.dump(sample_registered_calendars, outfile)
+    with open("events.json", "w") as outfile:
+        json.dump(sample_events, outfile)
+
+if __name__ == '__main__':
+    main()

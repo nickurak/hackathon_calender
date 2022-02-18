15a83ea7234628b94e639ef1090ca7598b7d189f
Jeremy Nickurak <jnickurak@dotdash.com>
2022-02-17 22:53:15 -0700

    db.py: helpers to get/save reminders

--- db.py
+++ db.py
@@ -28,6 +28,11 @@ def get_calendars():
                 'url': url
             }
 
+def get_reminders():
+    with open('reminders.json', "r") as rem_file:
+        data = json.load(rem_file)
+        for reminder in data:
+            yield reminder
 
 def write_calendars(calendars):
     with open("calendars.json", "w") as outfile:
@@ -37,4 +42,10 @@ def write_calendars(calendars):
 def write_events(events):
     with open("events.json", "w") as outfile:
         json.dump(events, outfile)
+        outfile.write("\n")
+
+
+def write_reminders(reminders):
+    with open("reminders.json", "w") as outfile:
+        json.dump(reminders, outfile)
         outfile.write("\n")

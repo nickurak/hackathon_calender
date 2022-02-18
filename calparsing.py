6c8c9245f174f57987450a6a20b8d61111eaa2f5
Jeremy Nickurak <jnickurak@dotdash.com>
2022-02-18 11:04:07 -0700

    calparsing: whitespace updates

--- calparsing.py
+++ calparsing.py
@@ -6,38 +6,37 @@ from datetime import timezone
 events = []
 
 def read_calender_events(calendar):
-  r = requests.get(url = calendar['url'])
-  
-  gcal = Calendar.from_ical(r.text)
-  
-  for component in gcal.walk():
-    if component.name == "VEVENT":
-      event = {}
-      event['calendar'] = calendar['name']
-      event['event_name'] = component.get('summary')
-      event['uid'] = component.get('uid')
-      event['start_time'] = component.get('dtstart').dt.replace(tzinfo=timezone.utc).timestamp()
-      event['end_time'] = component.get('dtend').dt.replace(tzinfo=timezone.utc).timestamp()
-      events.append(event)
-      
+    r = requests.get(url = calendar['url'])
+
+    gcal = Calendar.from_ical(r.text)
+
+    for component in gcal.walk():
+        if component.name == "VEVENT":
+            event = {}
+            event['calendar'] = calendar['name']
+            event['event_name'] = component.get('summary')
+            event['uid'] = component.get('uid')
+            event['start_time'] = component.get('dtstart').dt.replace(tzinfo=timezone.utc).timestamp()
+            event['end_time'] = component.get('dtend').dt.replace(tzinfo=timezone.utc).timestamp()
+            events.append(event)
+
 
 def read_allcalendars():
-  for calendar in get_calendars():
-          read_calender_events(calendar)
-            
-  write_events(events)
+    for calendar in get_calendars():
+        read_calender_events(calendar)
+
+    write_events(events)
 
 def read_calendar(cname):
-  
-  for calendar in get_calendars():
-    if cname == calendar['name']:
-      read_calender_events(calendar)
-            
-  write_events(events)
+    for calendar in get_calendars():
+        if cname == calendar['name']:
+            read_calender_events(calendar)
+
+    write_events(events)
 
 
 def main():
-  read_allcalendars()
+    read_allcalendars()
 
 if __name__ == '__main__':
     main()
